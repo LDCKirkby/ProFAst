bee7bd49970025728bba5da0f68ed31bebeb5544
LukeKirkby <luke.kirkby@icrar.org>
2024-02-29 13:16:24 +0800

    Update Done.R

--- Done.R
+++ Done.R
@@ -2,25 +2,29 @@ library(pracma)
 library(data.table)
 
 #Uncomment to create ordered heading csv
-bearings = as.data.frame(list.files(path = paste0(pwd()), pattern = "_"))
+done = as.data.frame(list.files(path = paste0(pwd()), pattern = "_"))
+bearings = as.data.frame(list.files(path = "/Volumes/WAVES/waves/wavesdata/kids/dr5/preprocessed/", pattern = ".5_g_"))
 colnames(bearings) <- c("RA_Dec")
+colnames(done) <- c("RA_Dec")
+RA_done = c()
+Dec_done = c()
 RA = c()
 Dec = c()
+bruh = c()
 for (i in 1:length(bearings$RA_Dec)){
   cat(bearings$RA_Dec[[i]],"\n")
-  RA = append(RA,strsplit(bearings$RA_Dec[[i]], split = "_")[[1]][1])
-  Dec = append(Dec, strsplit(bearings$RA_Dec[[i]], split = "_")[[1]][2])
-  #bearings[i] =
+  ra = strsplit(bearings$RA_Dec[[i]], split = "_")[[1]][2]
+  dec = strsplit(bearings$RA_Dec[[i]], split = "_")[[1]][3]
+  if(paste0(ra,"_",dec) %in% done$RA_Dec){
+    next
+  }
+  RA = append(RA,ra)
+  Dec = append(Dec, dec)
 
 }
-#colnames(bearings) <- c("RA", "Dec")
 
 RA_Dec = cbind(RA, Dec)
-
-RA_Dec = as.data.frame(RA_Dec)
 colnames(RA_Dec) = c("RA", "Dec")
 
-#RA_Dec = RA_Dec[order(RA_Dec$Dec, decreasing = TRUE),]
 
-
-write.csv(RA_Dec, paste0(pwd(),"/done.csv"))
+write.csv(RA_Dec, paste0(pwd(),"/todo.csv"))
