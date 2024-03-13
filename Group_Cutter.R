f67142205963cee29c8d896be2122b1a872ad3c0
LukeKirkby <luke.kirkby@icrar.org>
2024-03-13 14:48:37 +0800

    Update Group_Cutter.R

--- Group_Cutter.R
+++ Group_Cutter.R
@@ -112,7 +112,7 @@ for(i in 1:length(asteroids$groupID)){
   locut = c(kids, kids, kids)
   
   cat("Time to start printing images!\n")
-  Rwcs_imageRGB(R=cutim_r,G=cutim_g,B=cutim_Z, Rheader=r_image$header,Gheader=r_image$header,Bheader=Z_image$header, xlab="Right Ascension (deg)",ylab="Declination (deg)",coord.type="deg",locut=locut, hicut=c(kids,kids,kids) ,type="num",dowarp=FALSE, hersh = FALSE, grid = TRUE)
+  Rwcs_imageRGB(R=cutim_r,G=cutim_g,B=cutim_Z, Rkeyvalues=r_image$header,Gkeyvalyes=g_image$header,Bkeyvalues=Z_image$header, xlab="Right Ascension (deg)",ylab="Declination (deg)",coord.type="deg", dowarp=FALSE)#,locut=locut, hicut=c(kids,kids,kids) ,type="num", dowarp=FALSE)#, hersh = FALSE, grid = TRUE)
   #contplot(galgroupIDs,cutseg_dilate$image,"purple",wid,2)
   
   contplot(galgroupIDs,cutgroup_dilate$image, "skyblue", wid,4)
