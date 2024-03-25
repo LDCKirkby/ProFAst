f7db43c39ae42e4d0053eeec80503dd887b02ee7
LukeKirkby <luke.kirkby@icrar.org>
2024-03-25 16:58:11 +0800

    Update MPC_Imager.R

--- MPC_Imager.R
+++ MPC_Imager.R
@@ -24,22 +24,14 @@ Group_Cutter <- function(loc){
   }
   dir.create(paste0("./",loc,"/MPC_Images/"))
   
-  
-  
-  for(co in c("green","red","blue")){
-    
-  #Read in asteroid data
-  cat("Reading in asteroid data\n")
-  astcheck = read.table(paste0("./",loc,"/",loc,"_MPC_",co,".txt"), header = FALSE, na.strings = " ", col.names = c("ID", "RA", "Dec", "mag", "dRA/dt", "dDec/dt"))
-  
   cat("Reading in segmentation map data\n")
-
+  
   segim <- as.matrix(read.csv(paste0("./",loc,"/segim.csv")))
   
   cat("Generating groupim\n")
   groupim <- profoundSegimGroup(segim = segim)
   
-
+  
   cat("Loading images as pointers\n")
   g_image = Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed/KIDS_",loc,"_g_DMAG.fits"),header=TRUE,ext=1)
   r_image_input= Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed/KIDS_",loc,"_r_DMAG.fits"),header=TRUE,ext=1)
@@ -54,6 +46,19 @@ Group_Cutter <- function(loc){
   kids=(0.339^2)*(10^(0.4*(0-mulim)))
   viking=(0.339^2)*(10^(0.4*(30-mulim)))
   
+  
+  colours = c("green", "red", "blue")
+  headers = c(g_image$header, r_image$header, i_image$header)
+  keyvaluess = c(g_image$keyvalues, r_image$keyvalues, i_image$keyvalues)
+  for(i in 1:3){
+  co = colours[i]
+  header = headers[i]
+  keyvalues = keyvaluess[i]
+  #Read in asteroid data
+  cat("Reading in asteroid data\n")
+  astcheck = read.table(paste0("./",loc,"/",loc,"_MPC_",co,".txt"), skip = 4, na.strings = " ", col.names = c("ID", "RA", "Dec", "mag", "dRA/dt", "dDec/dt"))
+  
+
   ######################################################################
   cat("Begin iterating through astcheck\n")
   for(i in 1:length(astcheck$ID)){
@@ -63,18 +68,20 @@ Group_Cutter <- function(loc){
       
     galradec = astcheck[i, c("RA", "Dec")]
 
-    galpos=as.integer(Rwcs_s2p(RA=galradec$RA,Dec=galradec$Dec, keyvalues=g_image$keyvalues, EQUINOX = 2000L, RADESYS = "ICRS"))
+    galpos=as.integer(Rwcs_s2p(RA=galradec$RA,Dec=galradec$Dec, keyvalues=keyvalues))#, EQUINOX = 2000L, RADESYS = "ICRS"))
 
     box=c(2*wid,2*wid)
     cutim_g=g_image[galpos,box=box]
     cutim_r=r_image[galpos,box=box]
     cutim_i=i_image[galpos,box=box]
     
-    cutgroup_dilate=magcutout(image = groupim$groupim, header=g_image$header, loc=as.numeric(galpos),box=box,loc.type="image")
+    cutgroup_dilate=magcutout(image = groupim$groupim, header=header, loc=as.numeric(galpos),box=box,loc.type="image")
     
     decoff=2*(wid*0.339/3600.0)
     raoff=2*(wid*0.339/3600.0)/cos(galradec$Deccen*0.01745329)
     
+    groupimage = cutgroup_dilate$image
+    groupID = which(groupimage != 0)
     groupID=astcheck[astcheck$RAcen > galradec$RAcen - raoff & astcheck$RAcen < galradec$RAcen + raoff & astcheck$Deccen > galradec$Deccen - decoff & astcheck$Deccen < galradec$Deccen + decoff,"groupID"]
     
     cat("Printing ",ID," postage stamp\n")
@@ -89,8 +96,6 @@ Group_Cutter <- function(loc){
     
     #contplot(groupID,cutseg_dilate$image,"purple",wid,2)
     #contplot(groupID, i=NULL, cutgroup_dilate$image, "skyblue", header = image_header)
-    groupimage = cutgroup_dilate$image
-    groupIDs = which(groupimage != 0)
     
     for(groupID in groupimage){
       
@@ -118,8 +123,8 @@ Group_Cutter <- function(loc){
     }
     
 
-    pix_loc = radec2xy(astcheck$RA, astcheck$Dec, header = g_image$header, col = go)
-    points(x = pix_loc[[1]], y = pix_loc[[2]], add = TRUE, col = co, pch = 11)
+    pix_loc = radec2xy(astcheck$RA, astcheck$Dec, header = header)
+    points(pix_loc, add = TRUE, col = co, pch = 11)
     text(1,2*wid-50, label=paste0("MPC ID=",ID), col = co, cex=2.0, pos=4, add = TRUE)
       
     dev.off()
