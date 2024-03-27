d8945a73f182b46e5da5251ce0356c43adf175b0
LukeKirkby <luke.kirkby@icrar.org>
2024-03-27 14:01:12 +0800

    Update Find_Orb_Formatter.R

--- Find_Orb_Formatter.R
+++ Find_Orb_Formatter.R
@@ -11,7 +11,7 @@ obs_times = read.delim("./obs_times_full.txt", header =FALSE, col.names = c("fra
 asteroids = read.csv(paste0("./",loc,"/",loc,"_Asteroids.csv"))
 
 exposure = 0 
-half_year = c("A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y")
+# half_year = c("A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y")
 
 find_orb = c()
 astcheck = c()
@@ -31,14 +31,17 @@ for( i in 1:length(asteroids$groupID)){
   
   
   obs = subset(obs_times, subset = grepl(paste0(loc,"_",asteroids$Colour[i]), obs_times$frame) == TRUE & grepl("i2", obs_times$frame) ==FALSE)
-  obs_start = as.POSIXct(obs$obs1)
-  obs_end = as.POSIXct(obs$obs5) + (exposure/5)
-  paste0(year(obs_start), " ", month(obs_start), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
-  paste0(year(obs_end), " ", month(obs_end), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
-  
-  
-  
-  
+  obs_start = as.POSIXct(obs$obs1, tz = "UTC")
+  obs_end = as.POSIXct(obs$obs5, tz = "UTC") + (exposure/5)
+  if(month(obs_start) < 10){
+    ymd_start = paste0(year(obs_start), " ", paste0("0",month(obs_start)), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
+    ymd_end = paste0(year(obs_end), " ", paste0("0",month(obs_start)), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
+  }else{
+    ymd_start = paste0(year(obs_start), " ", month(obs_start), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
+    ymd_end = paste0(year(obs_end), " ", month(obs_end), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
+    
+  }
+ 
   #Uncomment once the max & min RA_Dec finder works properly 
   RA_top = paste0(deg2hms(asteroids[i, "tr_RA"])[[1]], " ",deg2hms(asteroids[i, "tr_RA"])[[2]], " ", deg2hms(asteroids[i, "tr_RA"])[[3]])
   Dec_top = paste0(deg2dms(asteroids[i, "tr_Dec"])[[1]], " ",deg2dms(asteroids[i, "tr_Dec"])[[2]], " ",substr(deg2dms(asteroids[i, "tr_Dec"])[[3]][], 1,4))
@@ -58,52 +61,52 @@ for( i in 1:length(asteroids$groupID)){
       spacer1 = paste0(spacer1," ")
     }
   }
-  line = paste0("     ",asteroids$groupID[[i]],spacer1, "  ", "C",ymd_start, substr(time_start, 2, 6), " ", RA_top, " ", Dec_top,"                      X11")
-  line2 = paste0("     ",asteroids$groupID[[i]],spacer1, "  ", "C",ymd_end, substr(time_end, 2, 6), " ", RA_bottom, " ", Dec_bottom,"                      X11")
+  line = paste0("     ",asteroids$groupID[[i]],spacer1, "  ", "C",ymd_start, " ", RA_top, " ", Dec_top,"                      X11")
+  line2 = paste0("     ",asteroids$groupID[[i]],spacer1, "  ", "C",ymd_end, " ", RA_bottom, " ", Dec_bottom,"                      X11")
   find_orb <- append(find_orb, line)
   find_orb <- append(find_orb, line2)
-  print(nchar(line))
+  cat(nchar(line), nchar(line2))
 }
 
-
-
-
-for( i in 1:length(asteroids$groupID)){
-  colour = asteroids$Colour[i]
-  
-  if("g" %in% colour == TRUE){
-    exposure = 900 #seconds
-  }
-  if("i" %in% colour == TRUE){
-    exposure = 1080 #seconds
-  }
-  if("r" %in% colour == TRUE){
-    exposure = 1800 #seconds
-  }
-  
-  
-  obs = subset(obs_times, subset = grepl(paste0(loc,"_",asteroids$Colour[i]), obs_times$frame) == TRUE & grepl("i2", obs_times$frame) ==FALSE)
-  obs_start = as.POSIXct(obs$obs1)
-  obs_end = as.POSIXct(obs$obs5) + (exposure/5)
-  obs_start = paste0(year(obs_start), " ", month(obs_start), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
-  obs_end = paste0(year(obs_end), " ", month(obs_end), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
-  
-  if()
-  
-  
-  RA_cen = paste0(deg2hms(asteroids[i, "RAcen"])[[1]], " ",deg2hms(asteroids[i, "RAcen"])[[2]], " ", deg2hms(asteroids[i, "RAcen"])[[3]])
-  Dec_cen = paste0(deg2dms(asteroids[i, "Deccen"])[[1]], " ",deg2dms(asteroids[i, "Deccen"])[[2]], " ",substr(deg2dms(asteroids[i, "Deccen"])[[3]][], 1,4))
-  
-  
-  
-  line = paste0("     ","K24",, "  ", "C",obs_start, " ", RA_cen, " ", Dec_cen,"                      X11")
-  astcheck <- append(astcheck, line)
-
-}  
-
+# 
+# 
+# 
+# for( i in 1:length(asteroids$groupID)){
+#   colour = asteroids$Colour[i]
+#   
+#   if("g" %in% colour == TRUE){
+#     exposure = 900 #seconds
+#   }
+#   if("i" %in% colour == TRUE){
+#     exposure = 1080 #seconds
+#   }
+#   if("r" %in% colour == TRUE){
+#     exposure = 1800 #seconds
+#   }
+#   
+#   
+#   obs = subset(obs_times, subset = grepl(paste0(loc,"_",asteroids$Colour[i]), obs_times$frame) == TRUE & grepl("i2", obs_times$frame) ==FALSE)
+#   obs_start = as.POSIXct(obs$obs1)
+#   obs_end = as.POSIXct(obs$obs5) + (exposure/5)
+#   obs_start = paste0(year(obs_start), " ", month(obs_start), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
+#   obs_end = paste0(year(obs_end), " ", month(obs_end), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
+#   
+#   if()
+#   
+#   
+#   RA_cen = paste0(deg2hms(asteroids[i, "RAcen"])[[1]], " ",deg2hms(asteroids[i, "RAcen"])[[2]], " ", deg2hms(asteroids[i, "RAcen"])[[3]])
+#   Dec_cen = paste0(deg2dms(asteroids[i, "Deccen"])[[1]], " ",deg2dms(asteroids[i, "Deccen"])[[2]], " ",substr(deg2dms(asteroids[i, "Deccen"])[[3]][], 1,4))
+#   
+#   
+#   
+#   line = paste0("     ","K24",, "  ", "C",obs_start, " ", RA_cen, " ", Dec_cen,"                      X11")
+#   astcheck <- append(astcheck, line)
+# 
+# }  
+# 
 
 write.table(find_orb, paste0("./",loc,"/",loc,"_findorb.txt"), sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
-write.table(astcheck, paste0("./",loc,"/",loc,"_astcheck.txt"), sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
+# write.table(astcheck, paste0("./",loc,"/",loc,"_astcheck.txt"), sep = " ", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 
 
