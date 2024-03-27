15058309dc1eef1ae0d452e43a94267999e4f5a4
LukeKirkby <luke.kirkby@icrar.org>
2024-03-27 14:35:28 +0800

    Update Find_Orb_Formatter.R

--- Find_Orb_Formatter.R
+++ Find_Orb_Formatter.R
@@ -33,21 +33,15 @@ for( i in 1:length(asteroids$groupID)){
   obs = subset(obs_times, subset = grepl(paste0(loc,"_",asteroids$Colour[i]), obs_times$frame) == TRUE & grepl("i2", obs_times$frame) ==FALSE)
   obs_start = as.POSIXct(obs$obs1, tz = "UTC")
   obs_end = as.POSIXct(obs$obs5, tz = "UTC") + (exposure/5)
-  if(month(obs_start) < 10){
-    ymd_start = paste0(year(obs_start), " ", paste0("0",month(obs_start)), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
-    ymd_end = paste0(year(obs_end), " ", paste0("0",month(obs_start)), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
-  }else{
-    ymd_start = paste0(year(obs_start), " ", month(obs_start), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
-    ymd_end = paste0(year(obs_end), " ", month(obs_end), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
-    
-  }
- 
+  #ymd_start = paste0(year(obs_start), " ", month(obs_start), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
+  #ymd_end = paste0(year(obs_end), " ", month(obs_start), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
+  
   #Uncomment once the max & min RA_Dec finder works properly 
   RA_top = paste0(deg2hms(asteroids[i, "tr_RA"])[[1]], " ",deg2hms(asteroids[i, "tr_RA"])[[2]], " ", deg2hms(asteroids[i, "tr_RA"])[[3]])
   Dec_top = paste0(deg2dms(asteroids[i, "tr_Dec"])[[1]], " ",deg2dms(asteroids[i, "tr_Dec"])[[2]], " ",substr(deg2dms(asteroids[i, "tr_Dec"])[[3]][], 1,4))
   RA_bottom = paste0(deg2hms(asteroids[i, "bl_RA"])[[1]], " ",deg2hms(asteroids[i, "bl_RA"])[[2]], " ", deg2hms(asteroids[i, "bl_RA"])[[3]])
   Dec_bottom = paste0(deg2dms(asteroids[i, "bl_Dec"])[[1]], " ",deg2dms(asteroids[i, "bl_Dec"])[[2]], " ",substr(deg2dms(asteroids[i, "bl_Dec"])[[3]][], 1,4))
-
+  
   
   # RA_top = paste0(deg2hms(asteroids$RAmax[[i]])[[1]], " ",deg2hms(asteroids$RAmax[[i]])[[2]], " ", deg2hms(asteroids$RAmax[[i]])[[3]])
   # Dec_top = paste0(deg2dms(asteroids$Decmax[[i]])[[1]], " ",deg2dms(asteroids$Decmax[[i]])[[2]], " ",substr(deg2dms(asteroids$Decmax[[i]])[[3]][], 1,4))
@@ -56,13 +50,52 @@ for( i in 1:length(asteroids$groupID)){
   # 
   if(nchar(asteroids$groupID[[i]]) <= 8){
     diff = 8-nchar(asteroids$groupID[[i]])
-    spacer1 = ""
+    spacer1 = "     "
     for(j in 1:diff){
-      spacer1 = paste0(spacer1," ")
+      spacer1 = paste0(spacer1,"0")
     }
   }
-  line = paste0("     ",asteroids$groupID[[i]],spacer1, "  ", "C",ymd_start, " ", RA_top, " ", Dec_top,"                      X11")
-  line2 = paste0("     ",asteroids$groupID[[i]],spacer1, "  ", "C",ymd_end, " ", RA_bottom, " ", Dec_bottom,"                      X11")
+  ymd_start = paste0(year(obs_start)," ")
+  ymd_end = paste0(year(obs_end), " ")
+  day_start = ""
+  day_end = ""
+  
+  if(month(obs_start) < 10){
+    ymd_start = paste0(ymd_start, paste0("0",month(obs_start)), " ")
+    ymd_end = paste0(ymd_end, paste0("0",month(obs_end)), " ")
+  }else{
+    ymd_start = paste0(ymd_start, month(obs_start), " ")
+    ymd_end = paste0(ymd_end, month(obs_end), " ")
+  }
+  
+  if(day(obs_start) < 10){
+    day_start = paste0("0",day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
+    day_end = paste0("0",day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
+  }else{
+    day_start = paste0(day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
+    day_end = paste0(day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
+  }
+  
+  if(nchar(day_start) < 7){
+    day_start = paste0(day_start, "0")
+  }
+  
+  cat(day_end, nchar(day_end))
+  if(nchar(day_end) < 7){
+    day_end = paste0(day_end, "0")
+    cat(day_end)
+  }
+  ymd_start = paste0(ymd_start, day_start)
+  ymd_end = paste0(ymd_end, day_end)
+
+#   ymd_start = paste0(year(obs_start), " ", month(obs_start), " ", day(obs_start) + trunc((hour(obs_start)/24 + minute(obs_start)/(24*60) + second(obs_start)/(24*60*60))*10^4)/10^4)
+#   ymd_end = paste0(year(obs_end), " ", month(obs_end), " ", day(obs_end) + trunc((hour(obs_end)/24 + minute(obs_end)/(24*60) + second(obs_end)/(24*60*60))*10^4)/10^4)
+
+ 
+  
+  line  = paste0(spacer1,asteroids$groupID[[i]], "  C",ymd_start, " ", RA_top, " ", Dec_top,"                      X11")
+  line2 = paste0(spacer1,asteroids$groupID[[i]], "  C", ymd_end , " ", RA_top, " ", Dec_top,"                      X11")
+  
   find_orb <- append(find_orb, line)
   find_orb <- append(find_orb, line2)
   cat(i, nchar(line), nchar(line2), "\n")
