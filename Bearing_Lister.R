ccd54dac0729ad8db9ba0bb655c41a60e26ecbea
LukeKirkby <luke.kirkby@icrar.org>
2024-08-14 10:46:58 +0800

    Created Bearing_Lister.R

---
+++ Bearing_Lister.R
@@ -0,0 +1,52 @@
+library(peakRAM,quietly = TRUE)
+library(xtable, quietly = TRUE)
+library(dst,quietly=TRUE)
+library(celestial,quietly = TRUE)
+library(devtools,quietly = TRUE)
+library(Cairo,quietly = TRUE)
+library(Rfits,quietly = TRUE)
+library(Rwcs,quietly = TRUE)
+library(ProFound,quietly = TRUE)
+library(magicaxis,quietly = TRUE)
+library(data.table,quietly = TRUE)
+library(plotrix,quietly = TRUE)
+library(foreign,quietly = TRUE)
+library(MASS,quietly = TRUE)
+library(ProPane, quietly = TRUE)
+library(ggplot2, quietly = TRUE)
+library(dplyr, quietly = TRUE)
+library(gsubfn, quietly = TRUE)
+library(fs, quietly = TRUE)
+library(showtext, quietly = TRUE)
+showtext_auto()
+
+args = commandArgs(trailingOnly = TRUE)
+RA_DEC = as.character(args[[1]])
+computer = as.character(args[[2]])
+
+if("sabine" == tolower(computer)){
+  files = as.data.frame(list.files(path = "/Volumes/ThunderBay/WAVES/wavesdata/Wide/kids/dr5/preprocessed/", pattern = "_u_"))
+}else{
+  files = as.data.frame(list.files(path = "/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed/", pattern = "_u_"))
+}
+colnames(files) <- c("RA_Dec")
+RA = c()
+Dec = c()
+for (i in 1:length(files$RA_Dec)){
+  RA = append(RA,strsplit(files$RA_Dec[i], split = "_")[[1]][2])
+  Dec = append(Dec, strsplit(files$RA_Dec[i], split = "_")[[1]][3])
+
+}
+
+RA_Dec = cbind(RA, Dec)
+
+RA_Dec = as.data.frame(RA_Dec)
+colnames(RA_Dec) = c("RA", "Dec")
+
+RA_Dec = RA_Dec[order(RA_Dec$Dec, decreasing = TRUE),]
+
+for(j in 1:length(RA_Dec$Dec)){
+  bearings = append(bearings, paste0(RA_Dec$RA[[j]],"_",RA_Dec$Dec[[j]]))
+}
+
+write.csv(bearings, "./bearings.csv")
--- Detection.R
+++ Detection.R
@@ -28,30 +28,6 @@ library(fs, quietly = TRUE)
 library(showtext, quietly = TRUE)
 showtext_auto()
 
-
-#Uncomment to create ordered heading csv
-# bearings = as.data.frame(list.files(path = "/Volumes/WAVES/waves/wavesdata/kids/dr5/preprocessed/", pattern = "_u_"))
-# colnames(bearings) <- c("RA_Dec")
-# RA = c()
-# Dec = c()
-# for (i in 1:length(bearings$RA_Dec)){
-#   RA = append(RA,strsplit(bearings$RA_Dec[i], split = "_")[[1]][2])
-#   Dec = append(Dec, strsplit(bearings$RA_Dec[i], split = "_")[[1]][3])
-#   #bearings[i] = 
-#   
-# }
-# # colnames(bearings) <- c("RA", "Dec")
-# 
-# RA_Dec = cbind(RA, Dec)
-# 
-# RA_Dec = as.data.frame(RA_Dec)
-# colnames(RA_Dec) = c("RA", "Dec")
-# 
-# RA_Dec = RA_Dec[order(RA_Dec$Dec, decreasing = TRUE),]
-# 
-# write.csv(RA_Dec, "/Users/lkirkby/bearings.csv")
-
-
 args = commandArgs(trailingOnly = TRUE)
 RA_DEC = as.character(args[[1]])
 computer = as.character(args[[2]])
