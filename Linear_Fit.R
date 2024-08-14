b5669651ae7dd9bd1e55b8891816299e9308420a
LukeKirkby <luke.kirkby@icrar.org>
2024-08-14 10:38:14 +0800

    Update Linear_Fit.R

--- Linear_Fit.R
+++ Linear_Fit.R
@@ -30,7 +30,7 @@ cat("*****************  Generating groupim ***************** \n")
 groupim = profoundSegimGroup(segim = segim)
 
 cat("*****************  Loading images as pointers ***************** \n")
-g_image = Rfits_point(paste0("D:/Swap/Thesis/",loc,"/KIDS_",loc,"_g_DMAG.fits"),header=TRUE,ext=1)
+g_image = Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed/KIDS_",loc,"/KIDS_",loc,"_g_DMAG.fits"),header=TRUE,ext=1)
 r_image_input= Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed/KIDS_",loc,"_r_DMAG.fits"),header=TRUE,ext=1)
 i_image_input= Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed/KIDS_",loc,"_i1_DMAG.fits"),header=TRUE,ext=1)
 cat("*****************  Warping r&i frames ***************** \n")
