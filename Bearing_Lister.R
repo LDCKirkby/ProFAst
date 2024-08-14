02be6f891d3528e81816df1541d73b2001aac200
LukeKirkby <luke.kirkby@icrar.org>
2024-08-14 10:51:35 +0800

    Update Bearing_Lister.R

--- Bearing_Lister.R
+++ Bearing_Lister.R
@@ -47,5 +47,5 @@ bearings = c()
 for(j in 1:length(RA_Dec$Dec)){
   bearings = append(bearings, paste0(RA_Dec$RA[[j]],"_",RA_Dec$Dec[[j]]))
 }
-colname(bearings) = c("RA_Dec")
+colnames(bearings) = c("RA_Dec")
 write.csv(bearings, "./bearings.csv", quote = FALSE)
