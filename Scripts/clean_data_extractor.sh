5f6a2ba4f83b8eeccf96d1f41fdd58d7968f7f54
LukeKirkby <luke.kirkby@icrar.org>
2025-01-08 14:04:40 +0800

    Adjustments to clean_data_extractor.sh

--- Scripts/clean_data_extractor.sh
+++ Scripts/clean_data_extractor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -i
 
 for file in ./*_*.*; do
-    touch ./"$file"/"$file"_real_asteroids.txt
-    touch ./"$file"/"$file"_real_asteroid_data.csv
+    echo $file
+    touch ./"$file"/"$file"_real_asteroids.csv
 
-    for asteroid in ./"$file"/"$file"_real_asteroids.txt; do
+    for asteroid in ./Asteroid_Images/clean-list.txt; do
         RA=$(echo $asteroid | cut -d'_' -f 1)
         Dec=$(echo $asteroid | cut -d'_' -f 2)
         PNG=$(echo $asteroid | cut -d'_' -f 3)
         SEGID=$(echo $PNG | cut -d'.' -f 1)
-        echo $RA","$Dec","$SEGID >> ./"$file"/"$file"_real_asteroid_data.csv    
+        echo $RA","$Dec","$SEGID >> ./"$file"/"$file"_real_asteroids.csv    
     done    
 done
