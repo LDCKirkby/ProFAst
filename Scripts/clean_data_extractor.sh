229b2ca80f08c5e87bc300612c5ec156fb5700f6
LukeKirkby <luke.kirkby@icrar.org>
2025-01-08 14:25:10 +0800

    Trying with grep to speed it up

--- Scripts/clean_data_extractor.sh
+++ Scripts/clean_data_extractor.sh
@@ -2,16 +2,19 @@
 
 for file in *_*.*; do
     echo $file
-    touch ./"$file"/"$file"_real_asteroids.csv
+    REAL=$(./"$file"/"$file"_real_asteroids.csv)
+    touch $REAL
 
-    while read asteroid; do
-        RA=$(echo $asteroid | cut -d'_' -f 1)
-        Dec=$(echo $asteroid | cut -d'_' -f 2)
-        PNG=$(echo $asteroid | cut -d'_' -f 3)
-        SEGID=$(echo $PNG | cut -d'.' -f 1)
-        if [ $RA"_"$Dec = $file ]; then
-            echo $RA","$Dec","$SEGID
-            echo $RA","$Dec","$SEGID >> ./"$file"/"$file"_real_asteroids.csv    
-        fi
-    done <clean-list.txt
+    grep "$file" clean-list.txt > $REAL
+
+    #while read asteroid; do
+    #    RA=$(echo $asteroid | cut -d'_' -f 1)
+    #    Dec=$(echo $asteroid | cut -d'_' -f 2)
+    #    PNG=$(echo $asteroid | cut -d'_' -f 3)
+    #    SEGID=$(echo $PNG | cut -d'.' -f 1)
+    #    if [ $RA"_"$Dec = $file ]; then
+    #        echo $RA","$Dec","$SEGID
+    #        echo $RA","$Dec","$SEGID >> ./"$file"/"$file"_real_asteroids.csv    
+    #    fi
+    #done <clean-list.txt
 done
