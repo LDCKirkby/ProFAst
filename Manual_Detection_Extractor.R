54b7432b324308db3ece68404939beebfa42c3e7
LukeKirkby <luke.kirkby@icrar.org>
2025-01-20 10:58:30 +0800

    Typo

--- Manual_Detection_Extractor.R
+++ Manual_Detection_Extractor.R
@@ -7,4 +7,4 @@ filtered_data = read.csv(paste0(RA_Dec,"/",RA_Dec,"_N100_Filtered_Asteroids.csv"
 visual_asteroids = read.csv(paste0(RA_Dec,"/",RA_Dec,"_real_asteroids.csv"), header=TRUE)
 verified_asteroids = subset(filtered_data, filtered_data$segID %in% visual_asteroids$segID)
 
-write.csv(verified_asteroids, file=paste0(RA_Dec,"/",RA_Dec,".csv"))
+write.csv(verified_asteroids, file=paste0(RA_Dec,"/",RA_Dec,"_Verified.csv"))
