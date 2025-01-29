2f1a2313e380cca3b8bfedf21316e724db939880
LukeKirkby <luke.kirkby@icrar.org>
2025-01-29 18:31:24 +0800

    Adjusted manual detection extractor

--- Manual_Detection_Extractor.R
+++ Manual_Detection_Extractor.R
@@ -1,10 +1,10 @@
-
-
+library(stringr)
 args = commandArgs(trailingOnly = TRUE)
 RA_Dec = gsub("[\r\n]", "", as.character(args[[1]]))
 
 filtered_data = read.csv(paste0(RA_Dec,"/",RA_Dec,"_N100_Filtered_Asteroids.csv"))
 visual_asteroids = read.csv(paste0(RA_Dec,"/",RA_Dec,"_real_asteroids.csv"), header=TRUE)
-verified_asteroids = subset(filtered_data, filtered_data$segID %in% visual_asteroids$segID)
+asteroid_IDS = str_sub(visual_asteroids$segID, 2, -1)
+verified_asteroids = subset(filtered_data, filtered_data$segID %in% asteroid_IDS)
 
 write.csv(verified_asteroids, file=paste0(RA_Dec,"/",RA_Dec,"_Verified.csv"))
