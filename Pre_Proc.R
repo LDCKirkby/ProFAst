133c3dbffec57c2c49b5255aaeaccb665ac732dd
LukeKirkby <luke.kirkby@icrar.org>
2025-02-23 11:51:55 +0800

    .

--- Pre_Proc.R
+++ Pre_Proc.R
@@ -9,9 +9,9 @@ Pre_Proc <- function(loc, computer){
     #i2=Rfits_point(paste0("/Volumes/ThunderBay/WAVES/wavesdata/Wide/kids/dr5/preprocessed/KIDS_",loc,"_i2_DMAG.fits"), header=TRUE, ext=1)
   }else{
     cat(paste0("/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed"),"\n")
-    g=Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/VST/kids/dr5/preprocessed/KIDS_",loc,"_g_DMAG.fits"), header=TRUE, ext=1)
-    r=Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/VST/kids/dr5/preprocessed/KIDS_",loc,"_r_DMAG.fits"), header=TRUE, ext=1)
-    i1=Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/VST/kids/dr5/preprocessed/KIDS_",loc,"_i1_DMAG.fits"), header=TRUE, ext=1)
+    g=Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/VST/dr5/preprocessed/KIDS_",loc,"_g_DMAG.fits"), header=TRUE, ext=1)
+    r=Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/VST/dr5/preprocessed/KIDS_",loc,"_r_DMAG.fits"), header=TRUE, ext=1)
+    i1=Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/VST/dr5/preprocessed/KIDS_",loc,"_i1_DMAG.fits"), header=TRUE, ext=1)
     #i2=Rfits_point(paste0("/Volumes/WAVESSPD/waves/wavesdata/Wide/kids/dr5/preprocessed/KIDS_",loc,"_i2_DMAG.fits"), header=TRUE, ext=1)
   }
   
