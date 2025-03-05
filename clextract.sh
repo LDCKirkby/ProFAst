8969dbc25d7f90c564857d578c82d361d405b8c6
LukeKirkby <luke.kirkby@icrar.org>
2025-03-05 12:49:00 +0800

    .

--- clextract.sh
+++ clextract.sh
@@ -5,13 +5,13 @@ for file in *_*.*; do
     touch "$file"/temp.txt
     grep "$file" $1 >> "$file"/temp.txt
     
-    echo "RA,Dec,segID" > "$file"/"$file"_Verified.csv
+    echo "RA,Dec,segID" > "$file"/"$file"_clean_asteroids.csv
     while read asteroid; do
        RA=$(echo $asteroid | cut -d'_' -f 1)
        Dec=$(echo $asteroid | cut -d'_' -f 2)
        PNG=$(echo $asteroid | cut -d'_' -f 3)
        SEGID=$(echo $PNG | cut -d'.' -f 1)
-       echo $RA","$Dec","$SEGID >> "$file"/"$file"_Verified.csv    
+       echo $RA","$Dec","$SEGID >> "$file"/"$file"_clean_asteroids.csv    
     done <"$file"/temp.txt
     rm "$file"/temp.txt
 done
