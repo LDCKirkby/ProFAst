e7944bec91699d6ca12c2f35bb36ca70e567f012
LukeKirkby <luke.kirkby@icrar.org>
2025-03-05 13:59:48 +0800

    .

--- Non/Manual_Detection_Extractor.R
+++ Non/Manual_Detection_Extractor.R
@@ -11,16 +11,17 @@ wrong_g = read.csv(paste0(RA_Dec,"/",RA_Dec,"_wrong_g.csv"), header = TRUE)
 wrong_r = read.csv(paste0(RA_Dec,"/",RA_Dec,"_wrong_r.csv"), header = TRUE)
 wrong_i = read.csv(paste0(RA_Dec,"/",RA_Dec,"_wrong_i.csv"), header = TRUE)
 
-if(length(wrong_g$segID) != 0){
 g_IDS = str_sub(wrong_g$segID, 2, -1)
+r_IDS = str_sub(wrong_r$segID, 2, -1)
+i_IDS = str_sub(wrong_i$segID, 2, -1)
+
+if(length(g_IDS) != 0){
 filtered_data[filtered_data$segID %in% g_IDS,]$Colour = "g"
 }
-if(length(wrong_r$segID) != 0){
-r_IDS = str_sub(wrong_r$segID, 2, -1)
+if(length(r_IDS) != 0){
 filtered_data[filtered_data$segID %in% r_IDS,]$Colour = "r"
 }
-if(length(wrong_i$segID) != 0){
-i_IDS = str_sub(wrong_i$segID, 2, -1)
+if(length(i_IDS) != 0){
 filtered_data[filtered_data$segID %in% i_IDS,]$Colour = "i"
 }
 
