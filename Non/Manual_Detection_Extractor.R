9f8288433fabf77bc9565f992609acab054027e3
LukeKirkby <luke.kirkby@icrar.org>
2025-03-05 13:55:57 +0800

    Upgraded Manual_Detection_Extractor.R to check for incorrect colour

--- Non/Manual_Detection_Extractor.R
+++ Non/Manual_Detection_Extractor.R
@@ -5,6 +5,26 @@ RA_Dec = gsub("[\r\n]", "", as.character(args[[1]]))
 filtered_data = read.csv(paste0(RA_Dec,"/",RA_Dec,"_N100_Filtered_Asteroids.csv"))
 visual_asteroids = read.csv(paste0(RA_Dec,"/",RA_Dec,"_clean_asteroids.csv"), header=TRUE)
 asteroid_IDS = str_sub(visual_asteroids$segID, 2, -1)
-verified_asteroids = subset(filtered_data, filtered_data$segID %in% asteroid_IDS)
 
+#Check for incorrectly coloured asteroids
+wrong_g = read.csv(paste0(RA_Dec,"/",RA_Dec,"_wrong_g.csv"), header = TRUE)
+wrong_r = read.csv(paste0(RA_Dec,"/",RA_Dec,"_wrong_r.csv"), header = TRUE)
+wrong_i = read.csv(paste0(RA_Dec,"/",RA_Dec,"_wrong_i.csv"), header = TRUE)
+
+if(length(wrong_g$segID) != 0){
+g_IDS = str_sub(wrong_g$segID, 2, -1)
+filtered_data[filtered_data$segID %in% g_IDS,]$Colour = "g"
+}
+if(length(wrong_r$segID) != 0){
+r_IDS = str_sub(wrong_r$segID, 2, -1)
+filtered_data[filtered_data$segID %in% r_IDS,]$Colour = "r"
+}
+if(length(wrong_i$segID) != 0){
+i_IDS = str_sub(wrong_i$segID, 2, -1)
+filtered_data[filtered_data$segID %in% i_IDS,]$Colour = "i"
+}
+
+verified_asteroids = subset(filtered_data, filtered_data$segID %in% asteroid_IDS | filtered_data$segID %in% g_IDS |
+                            filtered_data$segID %in% r_IDS | filtered_data$segID %in% i_IDS)
 write.csv(verified_asteroids, file=paste0(RA_Dec,"/",RA_Dec,"_Verified.csv"))
+
