0d041ca4561f1f4f703c6ea1e18b8780186749ef
LukeKirkby <luke.kirkby@icrar.org>
2025-03-13 13:51:45 +0800

    .

--- Core/Group_Remover.R
+++ Core/Group_Remover.R
@@ -29,3 +29,6 @@ Group_Remover.R <- function(RA_Dec){
     write.csv(no_dupes, paste0("./",RA_Dec,"/",RA_Dec,"_no_dupes.csv"), col.names=TRUE, row.names=FALSE)
     write.table(dupes$segID, paste0("./",RA_Dec,"/",RA_Dec,"_dupes.txt"), col.names=FALSE, row.names=FALSE)
 }
+
+args = commandArgs(trailingOnly = TRUE)
+RA_DEC = gsub("[\r\n]", "", as.character(args[[1]]))
