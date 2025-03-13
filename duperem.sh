7b203f3dd1974b05e861363db633cff24130bc35
LukeKirkby <luke.kirkby@icrar.org>
2025-03-13 14:12:46 +0800

    .

--- duperem.sh
+++ duperem.sh
@@ -4,11 +4,11 @@ for RA_Dec in *_*.*; do
     echo $RA_Dec
     
     while read dupes; do
-        echo rm $RA_Dec"/Linear_Fits/MPC_Format/*"$dupes"*"
-    #    RA=$(echo $asteroid | cut -d'_' -f 1)
-    #    Dec=$(echo $asteroid | cut -d'_' -f 2)
-    #    PNG=$(echo $asteroid | cut -d'_' -f 3)
-    #    SEGID=$(echo $PNG | cut -d'.' -f 1)
-    #    echo $RA","$Dec","$SEGID >> "$file"/"$file"_"$2".csv
+        rm $RA_Dec"/Linear_Fits/MPC_Format/*"$dupes"*"
+    #   RA=$(echo $asteroid | cut -d'_' -f 1)
+    #   Dec=$(echo $asteroid | cut -d'_' -f 2)
+    #   PNG=$(echo $asteroid | cut -d'_' -f 3)
+    #   SEGID=$(echo $PNG | cut -d'.' -f 1)
+    #   echo $RA","$Dec","$SEGID >> "$file"/"$file"_"$2".csv
     done <$RA_Dec"/"$RA_Dec"_dupes.txt"
 done
