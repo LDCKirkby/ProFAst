79958cb021eaa10240783d2892e3cfa96201a6f2
LukeKirkby <luke.kirkby@icrar.org>
2025-05-08 20:05:20 +0800

    .

--- urgent.R
+++ urgent.R
@@ -32,73 +32,9 @@ RA_DEC = gsub("[\r\n]", "", as.character(args[[1]]))
 loc = RA_DEC
 computer = "simon"
 
-files = list.files(path=paste0(RA_DEC,"/"), pattern="Possible_Asteroids.csv")
-if(length(files) == 0){
-cat_groups = read.csv(paste0(RA_DEC,"/",RA_DEC,"_Filtered_Asteroids.csv"))
+Flux_Filter(RA_DEC)
 
-cat_groups$flux_gt = abs(cat_groups$flux_gt)
-cat_groups$flux_rxt = abs(cat_groups$flux_rxt)
-cat_groups$flux_i1xt = abs(cat_groups$flux_i1xt)
-
-#Extracting potential asteroids, based on their flux ratio
-cat("*********\n")
-cat("Beginning asteroid search\n")
-green_objects = cbind("Colour" = "g", subset(cat_groups, subset = cat_groups$flux_gt/(cat_groups$flux_rxt + cat_groups$flux_i1xt) >= 1))
-red_objects = cbind("Colour" = "r", subset(cat_groups, subset = cat_groups$flux_rxt/(cat_groups$flux_gt + cat_groups$flux_i1xt) >= 1))
-blue_objects = cbind("Colour" = "i", subset(cat_groups, subset = cat_groups$flux_i1xt/(cat_groups$flux_gt + cat_groups$flux_rxt) >= 1))
-
-#Old Method, less concise than g/r+i, r/g+i, i/g+r but unsure how other will turn out
-# green_objects = cbind(subset(cat_groups, subset = cat_groups$flux_gt/cat_groups$flux_rxt>=8 | cat_groups$flux_gt/cat_groups$flux_i1xt>=8), "Colour" = "g")
-# red_objects = cbind(subset(cat_groups, subset = cat_groups$flux_rxt/cat_groups$flux_gt>=8 | cat_groups$flux_rxt/cat_groups$flux_i1xt>=8), "Colour" = "r")
-# blue_objects = cbind(subset(cat_groups, subset = cat_groups$flux_i1xt/cat_groups$flux_gt>=8 | cat_groups$flux_i1xt/cat_groups$flux_rxt>=8), "Colour" = "i")
-
-#Applies edge buffer to red and blue, since they've been extended artificially
-RA = as.numeric(strsplit(loc, "_")[[1]][[1]])
-Dec = as.numeric(strsplit(loc, "_")[[1]][[2]])
-
-#Useful to apply edge buffer since some frames are being artificially grown
-cat("Applying edge buffer\n")
-red_objects = rbind(red_objects[red_objects$RAcen >= (RA - 0.5 + 0.001) & red_objects$RAcen <= (RA + 0.5 - 0.001) & red_objects$Deccen >= (Dec-0.5 + 0.001) & red_objects$Deccen <= (Dec + 0.5 - 0.001),])
-blue_objects = rbind(blue_objects[blue_objects$RAcen >= (RA - 0.5 + 0.001) & blue_objects$RAcen <= (RA + 0.5 - 0.001) & blue_objects$Deccen >= (Dec-0.5 + 0.001) & blue_objects$Deccen <= (Dec + 0.5 - 0.001),])
-green_objects = rbind(green_objects[green_objects$RAcen >= (RA - 0.5 + 0.001) & green_objects$RAcen <= (RA + 0.5 - 0.001) & green_objects$Deccen >= (Dec-0.5 + 0.001) & green_objects$Deccen <= (Dec + 0.5 - 0.001),])
-
-#Bind final lists of objects together
-possible_asteroids <- rbind(blue_objects,green_objects,red_objects)
-print(length(possible_asteroids$groupID))
-
-cat("Writing to ", paste0("./", loc,"/Possible_Asteroids.csv"),"\n")
-cat("*********\n\n")
-
-write.csv(possible_asteroids, file = paste0("./",loc,"/",loc,"_Possible_Asteroids.csv"), row.names=FALSE)
-
-rm(blue_objects, green_objects, red_objects, possible_asteroids, cat_groups)
-gc()
 Axrat_Filter(RA_DEC)
-
-}else{
-possible_asteroids = read.csv(paste0("./",loc,"/Possible_Asteroids.csv"), fill = TRUE)
-cat("*********\n")
-cat("Beginning axial filtering\n")
-cat("*********\n\n")
-
-#Axrat filter
-filtered_asteroids = subset(possible_asteroids, axrat_gt <= 0.35 | axrat_rxt <= 0.35 | axrat_i1xt <= 0.35)
-filtered_asteroids = setorder(filtered_asteroids, "groupID")
-
-cat("*********\n")
-cat("Filtered to ", length(filtered_asteroids$axrat_gt), "potential asteroids\n")
-cat("*********\n\n")
-
-cat("*********\n")
-cat("Writing to ", paste0("./", loc,"/",loc,"_Filtered_Asteroids.csv"),"\n")
-cat("*********\n\n")
-
-write.csv(filtered_asteroids, file = paste0("./",loc,"/",loc,"_Filtered_Asteroids.csv"), row.names=FALSE)
-
-rm(possible_asteroids, filtered_asteroids) 
-gc()
-}
-
 
 N100_Filter(RA_DEC)
 
